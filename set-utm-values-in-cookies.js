151964ccdd8060cad482293c53a88d2d5e7b7978
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-10-26 17:52:31 +0100

    Update set-utm-values-in-cookies.js

--- set-utm-values-in-cookies.js
+++ set-utm-values-in-cookies.js
@@ -1,15 +1,28 @@
-	const siteURL = new URL(location.href)
-    var utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'gclid', 'fbclid', 'ttclid', 'li_fat_id', 'twclid'];
-  var campaignData = {};
+// Function to set a cookie
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (encodeURIComponent(value) || "") + expires + "; path=/";
+}
 
-    for (var i = 0; i < utmParams.length; i++) {
-      var paramValue = siteURL.searchParams.get(utmParams[i])
-      if (paramValue) {
-        campaignData[utmParams[i]] = paramValue;
-      }
-    }
+// Your existing code
+const siteURL = new URL(location.href);
+const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'gclid', 'fbclid', 'ttclid', 'li_fat_id', 'twclid'];
+const campaignData = {};
 
-    if (Object.keys(campaignData).length > 0) {
-      console.log("Setting cookie: utm_values");
-      setCookie('utm_values', JSON.stringify(campaignData), 365); // Store cookie for 365 days
-    }
+for (let i = 0; i < utmParams.length; i++) {
+  const paramValue = siteURL.searchParams.get(utmParams[i]);
+  if (paramValue) {
+    campaignData[utmParams[i]] = paramValue;
+  }
+}
+
+if (Object.keys(campaignData).length > 0) {
+  console.log("Setting cookie: utm_values");
+  setCookie('utm_values', JSON.stringify(campaignData), 365); // Store cookie for 365 days
+}
+
